46c38202f6cf83740d5776d9809d8b1554aa169d
Ridwan <ridwanishola97@gmail.com>
2020-07-06 00:23:54 +0100

    minor visual changes

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ class App extends Component {
 
     render() {
         return (
-            <div><h1>K Actualluy dasno</h1></div>
+            <div><h1>K Actualluy dasno yes</h1></div>
         );
     }
 }
